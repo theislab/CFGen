0fc55ab01d09e0729152fb37581d6855cdcd15ad
Alessandro Palma <di93zoy@gpu-005.ai.lrz.de>
2023-04-05 11:59:43 +0200

    Bug fix in  file ```paths.py```

--- celldreamer/paths.py
+++ celldreamer/paths.py
@@ -1,7 +1,7 @@
 import pathlib
 from pathlib import Path
 
-ROOT = Path(__file__).parent.resolve()
+ROOT = Path(__file__).parent.parent.resolve()
 
 PERT_DATA_DIR = ROOT / "datasets"
 EMBEDDING_DIR = ROOT / "embeddings"
