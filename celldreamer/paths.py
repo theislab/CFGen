45e37faf9d83f20fd51d1e18d5b5fc9d12bdbaf9
richtertill <till-richter@gmx.de>
2024-01-19 09:27:52 +0100

    Add HLCA and Neurips data + New path where everyone can write

--- celldreamer/paths.py
+++ celldreamer/paths.py
@@ -3,4 +3,6 @@ from pathlib import Path
 ROOT = Path(__file__).parent.parent.resolve()
 
 DATA_DIR = ROOT / "datasets"
+RAW_DATA_DIR = Path("/lustre/groups/ml01/projects/2024_scvdm_till.richter/datasets/raw")
+PROCESSED_DATA_DIR = Path("/lustre/groups/ml01/projects/2024_scvdm_till.richter/datasets/processed")
 TRAINING_FOLDER = ROOT / "project_folder"
